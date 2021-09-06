2225bbf2b2944e286d88c404edc19e9f3a736d38
gaofengpeng <gaofengpeng1@163.com>
2021-09-06 10:53:13 +0800

    加入签名文件，方便打包

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# BaseMVP
-###### 此项目为搭建基础MVP框架，其中主要对Model，View，Presenter进行封装为Base基类。从而方便后续的使用，只需直接继承对应的Base基类即可。
+# BsPatch
 
-##### 该项目分为两个分支，maser分支和mvp分支。其中mvp分支是基础的mvp，而master分支则是封装了RxJava+Retrofit的MVP模式。
+使用`bsdiff-4.3`版本，`bzip2-1.0.6`版本
 
-##### 另外，该项目中有两个Module，其中app为项目演示，提供了一个简单的案例来使用封装起来的mvp，而common则是对MVP的封装，用于存放Base基类。
+测试步骤：
+
+1. 根据`bsdiff-4.3`和`bzip-1.0.6`编译出`bsdiff`。
+2. 使用`as`打包出两个版本的`apk`，并安装运行旧版本
+3. 使用`./bsdiff old.apk new.apk patch`命令产生`patch`差分文件
+4. 将`patch`复制到`Android/data/com.study.bspatch/cache`目录
+5. 运行旧版本，点击更新即可生成新包`output.apk`
+
--- app/src/main/java/com/study/bspatch/main/MainActivity.kt
+++ app/src/main/java/com/study/bspatch/main/MainActivity.kt
@@ -3,30 +3,64 @@ package com.study.bspatch.main
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.study.basemvp.R
 import com.study.bspatch.PatchUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
 /**
  * @author gaofengpeng
- * @date 20121/9/3
+ * @date 2021/9/3
  */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mTvText: TextView
     private lateinit var mButton: Button
+    private lateinit var mButtonToast: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mTvText = findViewById(R.id.tv_myText)
         mButton = findViewById(R.id.btn_update)
+        mButtonToast = findViewById(R.id.btn_version)
         init()
     }
 
     private fun init() {
+        // 用于创建cache目录，方便将patch文件放在Android/data/com.study.bspatch/cache目录
+        try {
+            File(externalCacheDir!!.absolutePath)
+        } catch (ignore: Exception) {
+        }
+        // 可以修改这里文本，然后打包两个不同的安装包
+        mButtonToast.setOnClickListener {
+            Toast.makeText(this, "我现在是新版本了哦哈哈哈哈哈", Toast.LENGTH_SHORT).show()
+        }
+
         mButton.setOnClickListener {
-            mTvText.text = PatchUtils.md5sum(applicationInfo.sourceDir)
+            lifecycleScope.launch {
+                mTvText.text = "开始合并安装包..."
+                val last = System.currentTimeMillis()
+                val isSuccess = withContext(Dispatchers.IO) {
+                    PatchUtils.bsPatch(
+                        newFile = "$externalCacheDir/output.apk",
+                        oldFile = applicationInfo.sourceDir,
+                        patch = "$externalCacheDir/patch"
+                    )
+                }
+                if (isSuccess) {
+                    val cost = System.currentTimeMillis() - last
+                    mTvText.text = "合并完成，耗时: $cost ms"
+                } else {
+                    mTvText.text = "合并失败"
+                }
+            }
         }
     }
 }
